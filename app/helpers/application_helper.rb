d20c9c0b9c3a31147eb836792ff3dbfcac06b183
Chris Adebiyi <oluvvafemi@gmail.com>
2025-10-18 21:54:55 +0200

    Add navLink helper for active state highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def nav_link_class(controller_names)
+    controller_names = Array(controller_names)
+    base_class = "nav-link text-white"
+    controller_names.include?(controller_name) ? "#{base_class} active bg-dark" : base_class
+  end
 end
